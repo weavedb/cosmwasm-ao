f406f52445b1d756c337d2a984dc02d9d4504885
Tomoya Nagasawa <ocrybit@gmail.com>
2024-02-27 20:05:08 +0400

    AO units setup script

---
+++ ao/scripts/start.js
@@ -0,0 +1,31 @@
+const ArLocal = require("arlocal").default
+const Arweave = require("arweave")
+const MU = require("../mu")
+const SU = require("../su")
+const CU = require("../cu")
+
+const start = async (
+  _arweave = {
+    host: "localhost",
+    port: 1984,
+    protocol: "http",
+  },
+) => {
+  const arweave = Arweave.init(_arweave)
+  const wallet = await arweave.wallets.generate()
+  const addr = await arweave.wallets.jwkToAddress(wallet)
+  const arLocal = new ArLocal(1984, false)
+  await arLocal.start()
+  await arweave.api.get(`mint/${addr}/10000000000000000`)
+  console.log(`[Wallet] ${addr}`)
+  const mu = new MU()
+  await mu.init()
+  const su = new SU()
+  await su.init()
+  const cu = new CU()
+  await cu.init()
+
+  return { mu, su, cu, arweave, wallet, arLocal }
+}
+
+start()
