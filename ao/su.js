3e6e219541e8107f8fb5099a20a32b4463d3b543
Tomoya Nagasawa <ocrybit@gmail.com>
2024-03-24 20:44:00 +0400

    Assignment message type

--- ao/su.js
+++ ao/su.js
@@ -12,6 +12,12 @@ const {
 const { parse } = require("./utils")
 const Base = require("./base")
 
+function genHash(prev, current) {
+  return crypto
+    .createHash("sha256")
+    .update(prev + current)
+    .digest("hex")
+}
 class SU extends Base {
   constructor({
     port = 1986,
@@ -28,15 +34,17 @@ class SU extends Base {
     this.processes = {}
     this.epoch = 0
     this.nonce = 0
+    this.hash = ""
   }
   async init() {
     this.server.post("/", async (req, res) => {
       try {
         let type = null
+        let m = {}
         const timestamp = Date.now()
         const bundle = new Bundle(req.body)
         for (let v of bundle.items) {
-          const m = parse(v.tags)
+          m = parse(v.tags)
           if (m.read_only === "True") type = "read_only"
           const address = this.aob.owner(v)
           if (m.type === "Message") {
@@ -60,8 +68,26 @@ class SU extends Base {
         }
 
         if (type === "message") {
+          const current = bundle.items[0].id
+          this.hash = genHash(this.hash, current)
+          const tags = [
+            { name: "Data-Protocol", value: "cwao" },
+            { name: "Variant", value: "cwao.TN.1" },
+            { name: "Type", value: "Assignment" },
+            { name: "Process", value: Number(m.process).toString() },
+            { name: "Epoch", value: Number(++this.epoch).toString() },
+            { name: "Nonce", value: Number(++this.nonce).toString() },
+            { name: "Hash-Chain", value: this.hash },
+            { name: "Timestamp", value: Number(timestamp).toString() },
+            {
+              name: "Block-Height",
+              value: Number(
+                (await this.arweave.blocks.getCurrent()).height,
+              ).toString(),
+            },
+          ]
           const bdata = this.aob.nest(bundle)
-          const assignment = await this.aob.data({}, bdata.rawData)
+          const assignment = await this.aob.data({ tags }, bdata.rawData)
           const bundle2 = await this.aob.bundle([assignment])
           const { tx } = await this.aob.post(bundle2)
           res.status(201)
