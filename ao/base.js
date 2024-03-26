857018f9bf59a638fd46324cb76f048d2ace2fcb
Tomoya Nagasawa <ocrybit@gmail.com>
2024-03-26 08:13:43 +0400

    [cw20 demo] upgrade cwao

--- ao/base.js
+++ ao/base.js
@@ -2,8 +2,8 @@ const express = require("express")
 const { DataItem } = require("arbundles")
 const Arweave = require("arweave")
 const cors = require("cors")
-//const { AOBundles: AOB, Tag } = require("cwao")
-const { AOBundles: AOB, Tag } = require("../cwao-sdk")
+const { AOBundles: AOB, Tag } = require("cwao")
+
 class Base {
   constructor({ port, arweave, graphql, type, wallet, protocol, variant }) {
     this.graphql = graphql
