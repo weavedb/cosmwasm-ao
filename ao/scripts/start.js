dd65acebb8a4225e45a2f62de8f6ef6a7e282406
Tomoya Nagasawa <ocrybit@gmail.com>
2024-03-28 18:56:49 +0400

    persist arLocal state

--- ao/scripts/start.js
+++ ao/scripts/start.js
@@ -1,20 +1,23 @@
+const { resolve } = require("path")
 const ArLocal = require("arlocal").default
 const Arweave = require("arweave")
 const MU = require("../mu")
 const SU = require("../su")
 const CU = require("../cu")
 
-const start = async (
-  _arweave = {
+const start = async ({
+  network = {
     host: "localhost",
     port: 1984,
     protocol: "http",
   },
-) => {
-  const arweave = Arweave.init(_arweave)
+  dbPath = resolve(__dirname, ".db"),
+  persist = true,
+} = {}) => {
+  const arweave = Arweave.init(network)
   const wallet = await arweave.wallets.generate()
   const addr = await arweave.wallets.jwkToAddress(wallet)
-  const arLocal = new ArLocal(1984, false)
+  const arLocal = new ArLocal(1984, false, dbPath, persist)
   await arLocal.start()
   await arweave.api.get(`mint/${addr}/10000000000000000`)
   console.log(`[Wallet] ${addr}`)
